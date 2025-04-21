e3677cce755cf05d1a74e1309d290ff5d41395d5
WeiSengChung <wschung22@hotmail.com>
2025-04-21 13:07:01 +0800

    dasdsa

--- resources/js/components/Example4.js
+++ resources/js/components/Example4.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import {
     Table,
     Button,
+    Modal,
     ModalHeader,
     ModalBody,
     ModalFooter,
@@ -40,10 +41,10 @@ export default class Example extends Component {
 
     addPost() {
         axios
-            .post("http://127.0.0.1:8000/api/posts", this.state.newPostData)
+            .post("http://127.0.0.1:8000/api/post", this.state.newPostData)
             .then((response) => {
-                let { post } = this.state;
                 this.loadPost();
+                let { posts } = this.state;
                 this.setState({
                     posts,
                     newPostModal: false,
@@ -55,17 +56,118 @@ export default class Example extends Component {
                 });
             });
     }
-    componentWillMount(){
-        
+    componentWillMount() {
+        this.loadPost();
+    }
+    toggleNewPostModal() {
+        this.setState({
+            newPostModal: true,
+        });
     }
     render() {
+        let posts = this.state.posts.map((post) => {
+            return (
+                <tr key={post.id}>
+                    <td>{post.id}</td>
+                    <td>{post.title}</td>
+                    <td>{post.content}</td>
+                    <td>
+                        <Button color="success" size="sm" className="mr-2">
+                            Edit
+                        </Button>
+                        <Button color="danger" size="sm" className="mr-2">
+                            Delete
+                        </Button>
+                    </td>
+                </tr>
+            );
+        });
         return (
             <div className="container">
-                <h1>Example 4</h1>
+                <Button
+                    color="primary"
+                    onClick={this.toggleNewPostModal.bind(this)}
+                >
+                    Add Post
+                </Button>
+                <Modal
+                    isOpen={this.state.newPostModal}
+                    toggle={this.toggleNewPostModal.bind(this)}
+                >
+                    <ModalHeader toggle={this.toggleNewPostModal.bind(this)}>
+                        {" "}
+                        Add New Post
+                    </ModalHeader>
+                    <ModalBody>
+                        <FormGroup>
+                            <Label for="title">Title</Label>
+                            <Input
+                                id="title"
+                                value={this.state.newPostData.title}
+                                onChange={(e) => {
+                                    let { newPostData } = this.state;
+                                    newPostData.title = e.target.value;
+                                    this.setState({ newPostData });
+                                }}
+                            ></Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="content">Content</Label>
+                            <Input
+                                id="content"
+                                value={this.state.newPostData.content}
+                                onChange={(e) => {
+                                    let { newPostData } = this.state;
+                                    newPostData.content = e.target.value;
+                                    this.setState({ newPostData });
+                                }}
+                            ></Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="user_id">User ID</Label>
+                            <Input
+                                id="user_id"
+                                value={this.state.newPostData.user_id}
+                                onChange={(e) => {
+                                    let { newPostData } = this.state;
+                                    newPostData.user_id = e.target.value;
+                                    this.setState({ newPostData });
+                                }}
+                            ></Input>
+                        </FormGroup>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button
+                            color="primary"
+                            onClick={this.addPost.bind(this)}
+                        >
+                            Add Post{" "}
+                        </Button>{" "}
+                        <Button
+                            color="secondary"
+                            onClick={this.toggleNewPostModal.bind(this)}
+                        >
+                            {" "}
+                            Cancel{" "}
+                        </Button>
+                    </ModalFooter>
+                </Modal>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Content</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>{posts}</tbody>
+                </Table>
             </div>
         );
     }
 }
+
 if (document.getElementById("example")) {
     ReactDOM.render(<Example />, document.getElementById("example"));
 }
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,36 +1,36 @@
 @extends('layouts.auth')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
 
-                <div class="card-header">
+                    <div class="card-header">Dashboard</div>
 
-                
-               
-                    <form method='POST' action="/posts/">
-                        @csrf
-                        <label  for ="title"> Title </label >
-                        <input type ="text" name="title"><br>
-                        <label  for ="content"> Content </label >
-                        <input type ="text" name="content"><br>
-                        <input type ="hidden" name="user_id" value=0>
-                        @method('POST')
-                        <button type="submit" class="btn btn-danger float-right">
-                            Add Post
-                        </button>
-                    </form>
-                   
-                    
+                    <div class="card-header">
+
+
+
+                        <form method='POST' action="/api/post">
+                            @csrf
+                            @method('POST')
+                            <label for="title"> Title </label>
+                            <input type="text" name="title"><br>
+                            <label for="content"> Content </label>
+                            <input type="text" name="content"><br>
+                            <input type="hidden" name="user_id" value=0>
+                            <button type="submit" class="btn btn-danger float-right">
+                                Add Post
+                            </button>
+                        </form>
+
+
                     </div>
                 </div>
 
             </div>
         </div>
     </div>
-</div>
-</div>
+    </div>
+    </div>
 @endsection
